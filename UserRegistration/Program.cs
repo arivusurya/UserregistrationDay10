5c5c2ad82dd3aa45a70dd42517b4688640695ebe
arivusurya <arivusurya.3@gmail.com>
2023-05-22 21:35:45 +0530

    validate the firstname

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace UserRegistration{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            ValidateUser validateUser   = new ValidateUser();
+            string firstName = "Ares";
+            bool isvalid = validateUser.ValidateFirstName(firstName);
+
+            if(isvalid)
+            {
+                Console.WriteLine("{0} is valid" ,firstName);
+            }else{
+                Console.WriteLine("{0} is Not valid", firstName);
+            }
+        }
+    }
+}
---
+++ UserRegistration/ValidateUser.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System;
+namespace UserRegistration
+{
+    public class ValidateUser
+    {
+        public bool ValidateFirstName(string firstName)
+        {
+            string pattern = @"^[A-Z][a-zA-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(firstName);
+        }
+    }
+}
